b0992bd8445c1207544db4b42c6272bd055d092b
me <me@me.me.com>
2020-06-16 15:04:55 -0700

    implement test for final spec

--- __tests__/roman-numerals.test.js
+++ __tests__/roman-numerals.test.js
@@ -13,15 +13,17 @@ describe("appendMultiples",()=>{
 describe("decimalToRomanNumeral",()=>{
   test('Return for equivalents', () => {
     const equivalent = decimalToRomanNumeral(5);
-  
-    expect(equivalent).toEqual("V");
-   
+    expect(equivalent).toEqual("V"); 
   });
 
   test('Implement multiples -return simple equivalents such XX, C, MMM', () => {
     const equivalents = decimalToRomanNumeral(20);
-  
     expect(equivalents).toEqual("XX");
    
+  });
+
+  test('Handle any complex number', () => {
+    const complex = decimalToRomanNumeral(1956);
+    expect(complex).toEqual("MCMLVI");
   });
 });
