12ec939ef91978c5ab08e333132ab372488f7ae5
me <me@me.me.com>
2020-06-17 13:49:32 -0700

    tests pass after removing for loop

--- src/roman-numeral-library.js
+++ src/roman-numeral-library.js
@@ -3,7 +3,7 @@ let equivalentsMap = new Map([[1,"I"],[5,"V"],[10,"X"],[50,"L"],[100,"C"],[500,"
 let equivalentsMapKeys = [...equivalentsMap.keys()];
 
 export function findAppropriateMultiple(romanNumeralValue,decimal){ 
-  for(var i = 1; i <= 4;i++) {
+  for(let i = 1; i <= 4;i++) {
     if (romanNumeralValue * i > decimal) return i - 1;
   }
 }
@@ -13,19 +13,26 @@ export function appendMultiples(romanNumeralToMultiply,timesToMultipleRomanNumer
   return romanNumeralToMultiply + appendMultiples(romanNumeralToMultiply,timesToMultipleRomanNumeral-1);
 }
 
-export function findFirstRomanNumeralValueThatExceedsTheValueOfDecimal(decimal){
+export function findIndexOfValueThatExceedsTheValueOfDecimal(decimal){
   for (let i = 0; i < equivalentsMapKeys.length; i++){
     if((equivalentsMapKeys[i]) > decimal) {
-      return equivalentsMap.get(equivalentsMapKeys[i]);
+      console.log(i)
+      return i;
     }
   }
 }
 
+export function findFirstRomanNumeralValueThatExceedsTheValueOfDecimal(decimal){
+      let i = findIndexOfValueThatExceedsTheValueOfDecimal(decimal);
+      return equivalentsMap.get(equivalentsMapKeys[i]);
+}
+//referred to as decimal rather than inputted number because
+//this is a recursive function.
 export function decimalToRomanNumeral(decimal){
   if (decimal === 0) return "";
   if(decimal >3999) return "thats too high";
 
-  
+  let index = findIndexOfValueThatExceedsTheValueOfDecimal(decimal);
  
   let goesDownTwoWhenSubtracted = ["X","C","M"];
   
@@ -35,36 +42,32 @@ export function decimalToRomanNumeral(decimal){
   let totalDecimalOfMultiplies;
 
   let totalSubtractionDecimal;
-  let firstSubtractionRomanNumeral;
-  let secondSubtractionRomanNumeral;
-  for(var i = 0; i < equivalentsMapKeys.length; i++){
-    //find first roman numeral which exceeds the value of the decimal 
-    if((equivalentsMapKeys[i] > decimal)){
-      firstSubtractionRomanNumeral = equivalentsMap.get(equivalentsMapKeys[i]);
-      if (goesDownTwoWhenSubtracted.includes(firstSubtractionRomanNumeral)){
-        totalSubtractionDecimal = equivalentsMapKeys[i] - equivalentsMapKeys[i-2];
-        secondSubtractionRomanNumeral = equivalentsMap.get(equivalentsMapKeys[i-2]);
-      }
-      else{
-         secondSubtractionRomanNumeral = equivalentsMap.get(equivalentsMapKeys[i-1]);
-         totalSubtractionDecimal = equivalentsMapKeys[i] - equivalentsMapKeys[i-1]; 
-      }
-      //decide whether to to reiterate using append multiples or appendsubtraction
-      //if the current roman numeral subtract the previous roman numeral is greater than the decimal then we need to think in terms 
-      //of multiple roman numerals in a sequence from 1 to 3.
-      if(totalSubtractionDecimal > decimal){
-        romanNumeralToMultiply = equivalentsMap.get(equivalentsMapKeys[i-1]);
-        decimalOfRomanNumeralToMultiply = equivalentsMapKeys[i-1];
-        timesToMultipleRomanNumeral = findAppropriateMultiple(decimalOfRomanNumeralToMultiply,decimal);
-        totalDecimalOfMultiplies = timesToMultipleRomanNumeral * decimalOfRomanNumeralToMultiply;
-        return appendMultiples(romanNumeralToMultiply,timesToMultipleRomanNumeral) + decimalToRomanNumeral(decimal - totalDecimalOfMultiplies);
-      }
-      else{
-        //append lower value to higher value and then reiterate the function;
-        return (secondSubtractionRomanNumeral + firstSubtractionRomanNumeral) + decimalToRomanNumeral(decimal - totalSubtractionDecimal);
-      }
-    }
-  }
+
   
+  let firstSubtractionRomanNumeral = findFirstRomanNumeralValueThatExceedsTheValueOfDecimal(decimal);
+  let secondSubtractionRomanNumeral;
+
+
+  if (goesDownTwoWhenSubtracted.includes(firstSubtractionRomanNumeral)){
+    totalSubtractionDecimal = equivalentsMapKeys[index] - equivalentsMapKeys[index-2];
+    secondSubtractionRomanNumeral = equivalentsMap.get(equivalentsMapKeys[index-2]);
+  }
+  else{
+    secondSubtractionRomanNumeral = equivalentsMap.get(equivalentsMapKeys[index-1]);
+    totalSubtractionDecimal = equivalentsMapKeys[index] - equivalentsMapKeys[index-1]; 
+  }
+
+  if(totalSubtractionDecimal > decimal){
+    romanNumeralToMultiply = equivalentsMap.get(equivalentsMapKeys[index-1]);
+    decimalOfRomanNumeralToMultiply = equivalentsMapKeys[index-1];
+    timesToMultipleRomanNumeral = findAppropriateMultiple(decimalOfRomanNumeralToMultiply,decimal);
+    totalDecimalOfMultiplies = timesToMultipleRomanNumeral * decimalOfRomanNumeralToMultiply;
+    return appendMultiples(romanNumeralToMultiply,timesToMultipleRomanNumeral) + decimalToRomanNumeral(decimal - totalDecimalOfMultiplies);
+  }
+  else{
+    //append lower value to higher value and then reiterate the function;
+    return (secondSubtractionRomanNumeral + firstSubtractionRomanNumeral) + decimalToRomanNumeral(decimal - totalSubtractionDecimal);
+  }
+
 }
 
